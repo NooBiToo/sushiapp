117ed151c453ea13be81d4540342706bde82289c
Erl <mr.kaparov@gmail.com>
2021-05-01 23:02:49 +0800

    Updates

---
+++ precache-manifest.993e11440b097ebe632a5787285b8716.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4849af444e0d4f9189f34ba0316a95bf",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "670b2fe0155c94ed3014b2e83f58f51f",
+    "url": "/assets/icons/arrow-back.svg"
+  },
+  {
+    "revision": "500fd2a8b31aea06900536e183f0d386",
+    "url": "/assets/icons/homescreen144.png"
+  },
+  {
+    "revision": "29c7289aa5b6f9b320c012fca40367df",
+    "url": "/assets/icons/homescreen168.png"
+  },
+  {
+    "revision": "adbbc3010b74fb46e36218691be3983a",
+    "url": "/assets/icons/homescreen192.png"
+  },
+  {
+    "revision": "b4068be7cc82a0414926c847e4ec5ee4",
+    "url": "/assets/icons/homescreen48.png"
+  },
+  {
+    "revision": "d9ebf737f0f373aba26b1b1b4e8ee0f8",
+    "url": "/assets/icons/homescreen72.png"
+  },
+  {
+    "revision": "9e77160689aab7123d6f2301e3ca7532",
+    "url": "/assets/icons/homescreen96.png"
+  },
+  {
+    "revision": "ced1606d4de792f2866dfef53f7a0181",
+    "url": "/assets/icons/ico16.png"
+  },
+  {
+    "revision": "3aec6fb73c7fb5c2dd7cc9e435af5991",
+    "url": "/assets/icons/ico160.png"
+  },
+  {
+    "revision": "217b7097274bd6b985352de5194a9059",
+    "url": "/assets/icons/ico32.png"
+  },
+  {
+    "revision": "cb9eb29f93af9aaa0a5763f1a225c144",
+    "url": "/assets/images/bg.jpg"
+  },
+  {
+    "revision": "2f1f0e682cb027afe58a4444573f9c35",
+    "url": "/assets/images/rolls/Chiz-kani-maki.jpg"
+  },
+  {
+    "revision": "9b4e9c5382c4c858b00eb040f070155b",
+    "url": "/assets/images/rolls/Dabl-krem-chiz.jpg"
+  },
+  {
+    "revision": "7097727a70f2947080791b29481217da",
+    "url": "/assets/images/rolls/Ebi-nira.jpg"
+  },
+  {
+    "revision": "ae81b5847c9b3d508ea0f005efe2a030",
+    "url": "/assets/images/rolls/Ebi-tempura-maki.jpg"
+  },
+  {
+    "revision": "4d7d45388062603b27cf7033f7ae693b",
+    "url": "/assets/images/rolls/Filadelfiya-classic.jpg"
+  },
+  {
+    "revision": "d77959d914f34311259fa9d4d56d0e9a",
+    "url": "/assets/images/rolls/Kanada-layt.jpg"
+  },
+  {
+    "revision": "31b0525a1cd4fccfdb310f0f4e01d9c2",
+    "url": "/assets/images/rolls/Kappa-fila.jpg"
+  },
+  {
+    "revision": "ae9fe4174be7bf9a16f23c263d8927a0",
+    "url": "/assets/images/rolls/Midzu-tempura.jpg"
+  },
+  {
+    "revision": "13fc31ec08a45297002b6b5ea0b8ea99",
+    "url": "/assets/images/rolls/Murugay-maki.jpg"
+  },
+  {
+    "revision": "cdaffb8e9d7794e68566620d93fc4cf2",
+    "url": "/assets/images/rolls/Nomikomu-maki.jpg"
+  },
+  {
+    "revision": "fab41dddcfda877d3f4ba9e9062e245f",
+    "url": "/assets/images/rolls/Onaburi-maki.jpg"
+  },
+  {
+    "revision": "cb904c755c052c83602235c80a5cd32c",
+    "url": "/assets/images/rolls/Syake-maki.jpg"
+  },
+  {
+    "revision": "988f7a71916a22467782e7628f6b53cc",
+    "url": "/assets/images/rolls/Syake-oshizushi.jpg"
+  },
+  {
+    "revision": "bcec366ef29ef61034c5cb990bb5ad63",
+    "url": "/assets/images/rolls/TSezar-tempura-maki.jpg"
+  },
+  {
+    "revision": "551cb6a08d74b7b92298dcff0952a63f",
+    "url": "/assets/images/rolls/Tempura-pay.jpg"
+  },
+  {
+    "revision": "90f778b16e608e3d1bd6d6462ca50acc",
+    "url": "/assets/images/rolls/Tempura-salmonchiz-gril.jpg"
+  },
+  {
+    "revision": "e857019041014fc6918b68d3818c7025",
+    "url": "/assets/images/rolls/Tempura-syake.jpg"
+  },
+  {
+    "revision": "a169c719ccb592507d30b98e677bf577",
+    "url": "/assets/images/rolls/Tempura-tvist.jpg"
+  },
+  {
+    "revision": "5fa3a50c78fd8c2c2f2adac59343732c",
+    "url": "/assets/images/rolls/Tempura-unagi.jpg"
+  },
+  {
+    "revision": "c31840b551d513b720aeb56bf6d4cff3",
+    "url": "/assets/images/rolls/Unagi-khotto.jpg"
+  },
+  {
+    "revision": "261f4849035fe9594dceddc9b31fda6b",
+    "url": "/assets/images/rolls/ebi-murugay.jpg"
+  },
+  {
+    "revision": "93e81c009a9dcb989ca5ed6d47b6c6f4",
+    "url": "/assets/images/rolls/geisha.jpg"
+  },
+  {
+    "revision": "b7ebb1579e2fc86ce02c1566e06b2e19",
+    "url": "/assets/images/rolls/kaliforniya-classic.jpg"
+  },
+  {
+    "revision": "b47a53aedf513eea74232ca9678bef46",
+    "url": "/assets/images/rolls/kanada-classic.jpg"
+  },
+  {
+    "revision": "46dd700953d429b32244b0840ab52d2a",
+    "url": "/assets/images/rolls/mix-maki.jpg"
+  },
+  {
+    "revision": "1350d741e45f6b37d94d33934605f7b1",
+    "url": "/assets/images/rolls/roll.jpg"
+  },
+  {
+    "revision": "c21933c7e90fdcf67cc4b93dbc12cf98",
+    "url": "/assets/images/rolls/tempura-salmonsky.jpg"
+  },
+  {
+    "revision": "fd8c55a5ed50e706c80bf97245471e90",
+    "url": "/assets/images/rolls/tirashi-tempura.jpg"
+  },
+  {
+    "revision": "83e7621861092b2950aa5d9ae7cf6fe6",
+    "url": "/assets/images/sets/sakura_set.jpg"
+  },
+  {
+    "revision": "58ef7d2968bbf3252cf9f0facd18c7d1",
+    "url": "/assets/images/versal_logo-dark.svg"
+  },
+  {
+    "revision": "9a182547c91af1ec23e14cb1a87e0c9b",
+    "url": "/assets/images/versal_logo-light.svg"
+  },
+  {
+    "revision": "bd1802b15d7d759fcadcd017386e5728",
+    "url": "/assets/social.png"
+  },
+  {
+    "revision": "7d9b7a71f4aebc41d9c9",
+    "url": "/css/app.29d37277.css"
+  },
+  {
+    "revision": "af2a4a161a442004044e34ea266450c6",
+    "url": "/data/data.json"
+  },
+  {
+    "revision": "0b4c7fa0e1de16773165f5b4333ed393",
+    "url": "/fonts/PERTILI.0b4c7fa0.woff"
+  },
+  {
+    "revision": "2f0567a38289d7cc6e08a348381edf28",
+    "url": "/fonts/PERTILI.2f0567a3.woff2"
+  },
+  {
+    "revision": "a61bdb96d845a05a8e828c2c1b7ad13b",
+    "url": "/fonts/PERTILI.a61bdb96.eot"
+  },
+  {
+    "revision": "d2af138c50555e540a1aa475d0396871",
+    "url": "/fonts/PERTILI.d2af138c.ttf"
+  },
+  {
+    "revision": "1082eb3bf7be09cb7eea9fd3c057732e",
+    "url": "/fonts/TTNorms-Medium.1082eb3b.ttf"
+  },
+  {
+    "revision": "69acfd1e0ec986e2cbf935ec3a93766e",
+    "url": "/fonts/TTNorms-Medium.69acfd1e.eot"
+  },
+  {
+    "revision": "98c072155c25195df9b0015da6dbe52f",
+    "url": "/fonts/TTNorms-Medium.98c07215.woff"
+  },
+  {
+    "revision": "33f150c6ac81219f18b55864e087dd12",
+    "url": "/fonts/TTNorms-Regular.33f150c6.woff"
+  },
+  {
+    "revision": "c585352cc2be6277de0cb0adab7cbede",
+    "url": "/fonts/TTNorms-Regular.c585352c.eot"
+  },
+  {
+    "revision": "eb6edc3979c854b8a687a8b973dda56a",
+    "url": "/fonts/TTNorms-Regular.eb6edc39.ttf"
+  },
+  {
+    "revision": "5cb5cc112b6b9c35ad6ef05b83d8b9ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d9b7a71f4aebc41d9c9",
+    "url": "/js/app.db6cf81f.js"
+  },
+  {
+    "revision": "be16014971861ffacd4d",
+    "url": "/js/chunk-vendors.9ed0fea5.js"
+  },
+  {
+    "revision": "abd118ba2dc356c74dac64f9c878a2bf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
